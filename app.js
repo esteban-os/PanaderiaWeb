e778a332c6a84e9bcf3c69c389d16d9fb1ca4239
Esteban <eorosco.segovia@gmail.com>
2020-07-09 21:41:22 -0500

    actualización de proyecto web

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', function (req, res) {
-  res.send('<h1>Hello World!</h1>')
-})
+//configuracion
+app.use(express.static('public'));
 
+
+//servidor
 app.listen(port, function () {
   console.log('Example app listening on port 3000!')
 })
